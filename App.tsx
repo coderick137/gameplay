ccb2f86cb6ed9f5ef300fbcafd9d9e9a4caf1d0c
Ricardo Anjos <ricardoanj.14@gmail.com>
2023-04-06 14:57:20 -0300

    Implementação de fonst customizadas

--- App.tsx
+++ App.tsx
@@ -1,17 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useFonts } from 'expo-font';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+
 import { Singin } from './src/screens/Singin';
+import * as SplashScree from 'expo-splash-screen';
+import { StatusBar, View } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScree.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScree.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Singin />
+    <View
+      style={{ flex: 1 }}
+      onLayout={onLayoutRootView}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {
+        fontsLoaded ? <Singin /> : null
+      }
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
